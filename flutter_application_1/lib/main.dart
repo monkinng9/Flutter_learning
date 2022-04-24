95592308f2f1d72dc5400aca9379750a6c31f4a0
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-04-24 13:25:40 +0700

    Create the starter Flutter app

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,14 +9,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp (
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'), 
+          title: const Text('Welcome to Flutter'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('Hello World')
         )
       )
     );
