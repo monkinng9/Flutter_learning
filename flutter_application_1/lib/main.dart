06e87eef8372e3a62b6430f9648b1859c79240f6
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-04-28 05:47:38 +0700

    delete not necessary code

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/controller/authentication.dart';
+import 'package:flutter_application_1/ui/authentication.dart';
 import 'package:flutter_application_1/ui/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
