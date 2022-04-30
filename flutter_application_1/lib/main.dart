6bbe281dc105c6be01cb93f31137f77ea9e50f43
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-04-30 19:14:50 +0700

    Update Overall to prepare Step 03

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter_application_1/controller/authentication.dart';
 import 'package:flutter_application_1/ui/homePage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -35,4 +36,4 @@ class App extends StatelessWidget {
   }
 }
 
-class ApplicationState extends ChangeNotifier {}
+
--- flutter_application_1/lib/ui/homePage.dart
+++ flutter_application_1/lib/ui/homePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/controller/authentication.dart';
 import 'package:flutter_application_1/ui/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,6 +17,15 @@ class HomePage extends StatelessWidget {
           Image.asset('assets/codelab.png'),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
+          Consumer<ApplicationState>(),
+          const Divider(
+            thickness: 1,
+            height: 8,
+            color: Colors.grey,
+          ),
+          const Header("What we'll be doing"),
+          const Paragraph(
+              "Join Us for a day full of Firebase Workshops and Pizza"),
         ],
       ),
     );
